efd3f21c465d1e8033dc30c57649cc70533332d5
Mr.Licorice <p3pp1n0@gmail.com>
2022-10-24 12:46:58 +0200

    Cherry pick differences between base repo and keychron repo

--- tmk_core/protocol/chibios/chibios.c
+++ tmk_core/protocol/chibios/chibios.c
@@ -192,7 +192,8 @@ void protocol_pre_task(void) {
             /* Remote wakeup */
             if (suspend_wakeup_condition()) {
                 usbWakeupHost(&USB_DRIVER);
-                restart_usb_driver(&USB_DRIVER);
+                //restart_usb_driver(&USB_DRIVER);
+                usb_wakeup(&USB_DRIVER);
             }
         }
         /* Woken up */
